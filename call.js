4d2751639565fe42ada5dac61595695c53e7b6e1
AravindSI <aravindsivankalai99>
2022-11-07 19:46:37 +0530

    second

--- call.js
+++ call.js
@@ -1,42 +1,17 @@
-
-
-//call
-var obj={num:4};
-var mulToThis=function(a){
-    return this.num *a;
+//bind method
+let multiply=function(x,y){
+    console.log(x*y);
 }
-console.log(mulToThis.call(obj,8));
-
-//apply
-var obj={num:4};
-var mulToThis=function(a,b,c,d){
-    return this.num *a*b*c*d;
-}
-var arr=[2,3,4,5];
-console.log(mulToThis.apply(obj,arr));
-
-//bind
-var obj={num:4};
-var mulToThis=function(a,b,c,d){
-    return this.num *a*b*c*d;
-}
-var bound=mulToThis.bind(obj);
-console.log(bound(1,2,3,4));
-/*
- Create a new object called Student with age 20 , write a function which prints the age of the student from the student object.
-Dont pass in args. Read from 'this' and use BIND*/
-var student = {
-      age:20,
-    function () {
-        
-        return this.age;
+let multiplyByTwo=multiply.bind(this,3);
+multiplyByTwo(5);
+let multiplyByThree=multiply.bind(this,6);
+multiplyByThree(7);
+//closure method
+let multiply=function(x){
+    return function(y){
+        console.log(x*y);
     }
-};
+}
+let multiplyByThree=multiply(5);
+multiplyByThree(7);
 
-var printAge = function() {
-    console.log(this.age);
-};
-
-var AgeofStudent = printAge.bind(student);
-
-AgeofStudent();
